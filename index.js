76ac2e107a7d3dcbb3eb9fcdfc23bc5a4ba22873
Alexander Williams <williams_alex@protonmail.com>
2019-10-11 07:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ function appendKitten(name) {
   return new_array
 }
 
+function prependKitten(name) {
+  new_array = kittens
+  new_array.unshift(name)
+  return new_array
+}
+
+function removeLastKitten() {
+  new_array = kittens
+  new_array.pop()
+  return new_array
+}
+
+function removeFirstKitten() {
+  new_array = kittens
+  new_array.shift()
+  return new_array
+}
